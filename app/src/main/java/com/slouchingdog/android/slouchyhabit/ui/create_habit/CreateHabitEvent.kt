873a551b3336c01dbe09f56e68466c32b87acc08
slouchingdog <zvereva.karinaa@gmail.com>
2025-04-08 16:03:07 +0500

    enum fix

--- app/src/main/java/com/slouchingdog/android/slouchyhabit/ui/create_habit/CreateHabitEvent.kt
+++ app/src/main/java/com/slouchingdog/android/slouchyhabit/ui/create_habit/CreateHabitEvent.kt
@@ -2,5 +2,6 @@ package com.slouchingdog.android.slouchyhabit.ui.create_habit
 
 enum class CreateHabitEvent {
     ShowSnackBarSomeFieldsEmpty,
-    SaveHabitWithCorrectData
+    SaveHabitWithCorrectData,
+    PrefillFormWithPassedHabit
 }
